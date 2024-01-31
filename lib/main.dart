b9abb6153632e23940be1113815c98dd71256b73
southglory <devramyun@gmail.com>
2024-01-31 13:21:54 +0900

    getX
    Test GetXController as Reactive Controller

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,14 @@ class SimpleController extends GetxController {
   }
 }
 
+class ReactiveController extends GetxController {
+  RxInt counter = 0.obs;
+
+  void increase() {
+    counter++;
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -24,26 +32,59 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    Get.put(SimpleController());
-
+    Get.put(SimpleController()); // 단순 상태 관리 controller 등록
+    Get.put(ReactiveController()); // 반응형 상태 관리 controller 등록
     return Scaffold(
       appBar: AppBar(
-        title: const Text("단순 상태관리"),
+        title: const Text("단순 / 반응형 상태관리"),
       ),
       body: Center(
-        child: GetBuilder<SimpleController>(
-          builder: (controller) {
-            return ElevatedButton(
-              child: Text(
-                '현재 숫자: ${controller.counter}',
-              ),
-              onPressed: () {
-                controller.increase();
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GetBuilder<SimpleController>( // 단순 상태 관리
+              builder: (controller) {
+                return ElevatedButton(
+                  child: Text(
+                    '[단순]현재 숫자: ${controller.counter}',
+                  ),
+                  onPressed: () {
+                    controller.increase();
+                    // Get.find<SimpleController>().increase();
+                  },
+                );
               },
-            );
-          },
+            ),
+            GetX<ReactiveController>( // 반응형 상태관리 - 1
+              builder: (controller) {
+                return ElevatedButton(
+                  child: Text(
+                    '반응형 1 / 현재 숫자: ${controller.counter.value}', // .value 로 접근
+                  ),
+                  onPressed: () {
+                    controller.increase();
+                    // Get.find<ReactiveController>().increase();
+                  },
+                );
+              },
+            ),
+            Obx( // 반응형 상태관리 - 2
+                  () {
+                return ElevatedButton(
+                  child: Text(
+                    '반응형 2 / 현재 숫자: ${Get.find<ReactiveController>().counter.value}', // .value 로 접근
+                  ),
+                  onPressed: () {
+                    Get.find<ReactiveController>().increase();
+                  },
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
