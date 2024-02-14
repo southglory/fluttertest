487980134fefe891868709b0922754bbfcfb4e13
southglory <devramyun@gmail.com>
2024-02-14 11:13:18 +0900

    텍스트 컨트롤러 캡처, 갤러리 저장.

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,115 @@
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:typed_data';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:gallery_saver/gallery_saver.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class SimpleController extends GetxController {
-  int counter = 0;
-
-  void increase() {
-    counter++;
-    update();
-  }
-}
-
-class ReactiveController extends GetxController {
-  static ReactiveController get to => Get.find();
-  RxInt counter = 0.obs;
-
+class MyApp extends StatefulWidget {
   @override
-  void onInit() {
-    once(counter, (_) {
-      print('once : $_이 처음으로 변경되었습니다.');
-    });
-    ever(counter, (_) {
-      print('ever : $_이 변경되었습니다.');
-    });
-    debounce(
-      counter,
-          (_) {
-        print('debounce : $_가 마지막으로 변경된 이후, 1초간 변경이 없습니다.');
-      },
-      time: Duration(seconds: 1),
-    );
-    interval(
-      counter,
-          (_) {
-        print('interval $_가 변경되는 중입니다.(1초마다 호출)');
-      },
-      time: Duration(seconds: 1),
-    );
-    super.onInit();
-  }
-
-
-  void increase() {
-    counter++;
-  }
-}
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: MyHomePage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _textEditingController = TextEditingController();
+  final GlobalKey _globalKey = GlobalKey();
+  ui.Image? _capturedImage;
+  Uint8List? _imageBytes;
+  String _displayText = '';
+  Widget? _displayedImageWidget;
+
+  Future<void> _captureImage() async {
+    RenderRepaintBoundary boundary = _globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    if (byteData != null) {
+      setState(() {
+        _capturedImage = image;
+        _imageBytes = byteData.buffer.asUint8List();
+        _displayedImageWidget = Image.memory(_imageBytes!);
+      });
+    }
+  }
+
+  Future<void> _saveImageToGallery() async {
+    if (_imageBytes == null) return;
+    final tempDir = await getTemporaryDirectory();
+    final file = await File('${tempDir.path}/image.png').writeAsBytes(_imageBytes!);
+    final result = await GallerySaver.saveImage(file.path, albumName: "YourAlbumName");
+    print("이미지 저장 성공: $result");
+  }
 
   @override
   Widget build(BuildContext context) {
-    Get.put(SimpleController()); // 단순 상태 관리 controller 등록
-    Get.put(ReactiveController()); // 반응형 상태 관리 controller 등록
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("단순 / 반응형 상태관리"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GetBuilder<SimpleController>( // 단순 상태 관리
-              builder: (controller) {
-                return ElevatedButton(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Text and Image Processing')),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _textEditingController,
+                  decoration: InputDecoration(hintText: 'Enter multiline text here'),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  style: TextStyle(fontSize: 22, color: Colors.black),
+                ),
+              ),
+              RepaintBoundary(
+                key: _globalKey,
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.transparent,
                   child: Text(
-                    '[단순]현재 숫자: ${controller.counter}',
+                    _textEditingController.text,
+                    style: TextStyle(fontSize: 22, color: Colors.black),
                   ),
-                  onPressed: () {
-                    controller.increase();
-                    // Get.find<SimpleController>().increase();
-                  },
-                );
-              },
-            ),
-            GetX<ReactiveController>( // 반응형 상태관리 - 1
-              builder: (controller) {
-                return ElevatedButton(
-                  child: Text(
-                    '반응형 1 / 현재 숫자: ${controller.counter.value}', // .value 로 접근
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Save the current text with line breaks
+                  setState(() {
+                    _displayText = _textEditingController.text;
+                  });
+                },
+                child: Text('Show Text'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _captureImage();
+                },
+                child: Text('Capture and Show Image'),
+              ),
+              ElevatedButton(
+                onPressed: _saveImageToGallery,
+                child: Text('Save Image to Gallery'),
+              ),
+              if (_displayText.isNotEmpty)
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(_displayText, // Display text with line breaks
+                      style: TextStyle(fontSize: 22, color: Colors.black)),
+                ),
+              if (_displayedImageWidget != null)
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      _displayedImageWidget!,
+                      Text('Width: ${_capturedImage?.width}, Height: ${_capturedImage?.height}'),
+                    ],
                   ),
-                  onPressed: () {
-                    controller.increase();
-                    // Get.find<ReactiveController>().increase();
-                  },
-                );
-              },
-            ),
-            Obx( // 반응형 상태관리 - 2
-                  () {
-                return ElevatedButton(
-                  child: Text(
-                    '반응형 2 / 현재 숫자: ${Get.find<ReactiveController>().counter.value}', // .value 로 접근
-                  ),
-                  onPressed: () {
-                    Get.find<ReactiveController>().increase();
-                  },
-                );
-              },
-            ),
-          ],
+                ),
+            ],
+          ),
         ),
       ),
     );
